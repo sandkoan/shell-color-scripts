385f7bcb404c31fff2ac92ed037b7380be0edff2
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2020-07-26 15:19:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# Simple installation script
+
 cd $HOME
 git clone https://github.com/sandkoan/shell-color-scripts.git
 cd shell-color-scripts
@@ -7,3 +11,10 @@ cp -rf colorscripts ~/bin/shell-color-scripts/
 cp colorscript.sh ~/bin/shell-color-scripts/colorscript
 cd $HOME
 rm -rf shell-color-scripts
+
+if [ "$0" == "zsh" ]
+then
+  echo PATH=$PATH:~/bin/shell-color-scripts:~/bin/shell-color-scripts/colorscripts >> ~/.zshrc
+else
+  echo PATH=$PATH:~/bin/shell-color-scripts:~/bin/shell-color-scripts/colorscripts >> ~/.bashrc 
+fi
