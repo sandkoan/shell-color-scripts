e67fd9d6a59212ee2f408d193d628457594ccd93
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2020-07-27 23:28:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd shell-color-scripts
 rm -rf ~/bin/shell-color-scripts || return 1
 mkdir -p ~/bin/shell-color-scripts
 cp -rf colorscripts ~/bin/shell-color-scripts/
-cp colorscript.sh ~/bin/shell-color-scripts/colorscript
+cp colorscript ~/bin/shell-color-scripts/colorscript
 cd $HOME
 rm -rf shell-color-scripts
 
