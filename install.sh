a13fd90473f474dec5830c07de93d12d10e4ea6f
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2020-08-09 11:36:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ cd shell-color-scripts
 rm -rf ~/bin/shell-color-scripts || return 1
 echo 'Removing any existing installation of shell-color-scripts...'
 
+echo
 mkdir -p ~/bin/shell-color-scripts
 echo Making $HOME/bin/shell-color-scripts directory if it does not exist...
 
+echo
 cp -rf colorscripts ~/bin/shell-color-scripts/
 cp colorscript ~/bin/shell-color-scripts/colorscript
 echo Copying colorscripts to $HOME/bin ...
@@ -20,11 +22,13 @@ cd $HOME
 # rm -rf shell-color-scripts
 
 echo "Installation complete."
+echo
 echo Adding $HOME/bin/shell-color-scripts and $HOME/bin/shell-color-scripts/colorscripts to .zshrc and .bashrc
 echo 'PATH=$PATH:~/bin/shell-color-scripts:~/bin/shell-color-scripts/colorscripts' >> ~/.zshrc
 echo 'PATH=$PATH:~/bin/shell-color-scripts:~/bin/shell-color-scripts/colorscripts' >> ~/.bashrc
 
-colorscript --help
-
+echo 
 echo "To run a random color script each time you open a terminal, add the following to your shell configuration file:"
 printf " ### RANDOM COLOR SCRIPT ### \ncolorscript random "
+echo
+printf "To get help, run \ncolorscript --help"
