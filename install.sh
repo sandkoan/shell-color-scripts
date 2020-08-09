82a57cce5af25fc52ea5683085c69ac07b0940f2
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2020-08-09 11:38:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ git clone https://github.com/sandkoan/shell-color-scripts.git
 cd shell-color-scripts
 
 rm -rf ~/bin/shell-color-scripts || return 1
+echo
 echo 'Removing any existing installation of shell-color-scripts...'
 
-echo
 mkdir -p ~/bin/shell-color-scripts
 echo Making $HOME/bin/shell-color-scripts directory if it does not exist...
 
-echo
 cp -rf colorscripts ~/bin/shell-color-scripts/
 cp colorscript ~/bin/shell-color-scripts/colorscript
 echo Copying colorscripts to $HOME/bin ...
@@ -31,4 +30,4 @@ echo
 echo "To run a random color script each time you open a terminal, add the following to your shell configuration file:"
 printf " ### RANDOM COLOR SCRIPT ### \ncolorscript random "
 echo
-printf "To get help, run \ncolorscript --help"
+printf "To get help, run: \ncolorscript --help"
